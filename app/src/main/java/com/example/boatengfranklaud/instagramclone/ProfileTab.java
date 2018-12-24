434016ef7a64e4901bd69beea7862c71e2ffa3f1
nanayaw50 <nana.yaw50@gmail.com>
2018-12-24 21:31:15 +0000

    Implemented profile and user tabs

--- app/src/main/java/com/example/boatengfranklaud/instagramclone/ProfileTab.java
+++ app/src/main/java/com/example/boatengfranklaud/instagramclone/ProfileTab.java
@@ -1,11 +1,24 @@
 package com.example.boatengfranklaud.instagramclone;
 
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
@@ -13,6 +26,9 @@ import android.view.ViewGroup;
  */
 public class ProfileTab extends Fragment {
 
+    private EditText name, bio, profession, hobbies, favSports;
+    private Button btnSaveInfo;
+    private ParseUser appUser = null;
 
     public ProfileTab() {
         // Required empty public constructor
@@ -23,7 +39,100 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        // initialize UI component on the xml file (fragment_profile_tab)
+        name = view.findViewById(R.id.profile_name);
+        bio = view.findViewById(R.id.profile_bio);
+        profession = view.findViewById(R.id.profile_profession);
+        hobbies = view.findViewById(R.id.profile_hobbies);
+        favSports = view.findViewById(R.id.profile_sports);
+
+        btnSaveInfo = view.findViewById(R.id.profile_btnSave);
+
+        // get the current app user
+        appUser = ParseUser.getCurrentUser();
+
+        // load the profile of current user
+        hasProfileDetails();
+
+        // TODO: more improvement can be made to include image and other functionality.
+
+        // save details to the parse server
+        btnSaveInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                appUser.put("ProfileName", name.getText().toString().trim());
+                appUser.put("ProfileBio", bio.getText().toString().trim());
+                appUser.put("ProfileProfession", profession.getText().toString().trim());
+                appUser.put("ProfileHobbies", hobbies.getText().toString().trim());
+                appUser.put("ProfileFavSports", favSports.getText().toString().trim());
+
+                // show progress dialog
+                final ProgressDialog progressDialog = new ProgressDialog(getContext());
+                progressDialog.setMessage("Updating profile, please wait...");
+                progressDialog.show();
+
+                appUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            progressDialog.dismiss(); // dismiss progress dialog
+                            clearWidgets(); // clear old values
+                            hasProfileDetails(); // load the updated values
+                            FancyToast.makeText(getContext(), appUser.getUsername() + " is signed successfully.",
+                                    FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+
+                        } else {
+                            FancyToast.makeText(getContext(), e.getLocalizedMessage(),
+                                    FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        return view;
     }
 
+    private void clearWidgets() {
+        name.setText("");
+        bio.setText("");
+        profession.setText("");
+        hobbies.setText("");
+        favSports.setText("");
+    }
+
+    private void hasProfileDetails() {
+        // load  profile of the current user and
+        // check for null object from the server
+        if (appUser.getString("ProfileName") != null) {
+            name.setText(appUser.getString("ProfileName"));
+        } else {
+            name.setText("");
+        }
+
+        if (appUser.getString("ProfileBio") != null) {
+            bio.setText(appUser.getString("ProfileBio"));
+        } else {
+            bio.setText("");
+
+        }
+        if (appUser.getString("ProfileProfession") != null) {
+            profession.setText(appUser.getString("ProfileProfession"));
+        } else {
+            profession.setText("");
+        }
+        if (appUser.getString("ProfileHobbies") != null) {
+            hobbies.setText(appUser.getString("ProfileHobbies"));
+        } else {
+            hobbies.setText("");
+        }
+        if (appUser.getString("ProfileFavSports") != null) {
+            favSports.setText(appUser.getString("ProfileFavSports"));
+        } else {
+            favSports.setText("");
+        }
+    }
 }
--- app/src/main/java/com/example/boatengfranklaud/instagramclone/UsersTab.java
+++ app/src/main/java/com/example/boatengfranklaud/instagramclone/UsersTab.java
@@ -1,17 +1,31 @@
 package com.example.boatengfranklaud.instagramclone;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UsersTab extends Fragment {
+    private ListView usersList;
+    private ArrayList arrayList;
+    private ArrayAdapter arrayAdapter;
 
 
     public UsersTab() {
@@ -23,7 +37,45 @@ public class UsersTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_users_tab, container, false);
+
+        // init listView
+        usersList = view.findViewById(R.id.users_listView);
+
+        // init arrayList
+        arrayList = new ArrayList<>();
+
+        // init array adapter and set constructor parameters
+        arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, arrayList);
+
+        // Parse query for users
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+
+        // show progress dialog
+        final ProgressDialog progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Loading users, please wait...");
+        progressDialog.show();
+
+        // exclude the current user from the list the user is using the app
+        parseQuery.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                if(e == null && users.size() > 0){
+
+                    for(ParseUser user : users){
+                        arrayList.add(user.getUsername());
+                    }
+
+                    // set adapter for the listView
+                    usersList.setAdapter(arrayAdapter);
+                    progressDialog.dismiss();
+                    usersList.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        // return statement
+        return view;
     }
 
 }
